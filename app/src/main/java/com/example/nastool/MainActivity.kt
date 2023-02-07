f77a1716952d962c0f96c23207fc7fbc0e9bcc53
叮叮当 <604054726@qq.com>
2023-02-07 23:28:15 +0800

    支持输入nastool网址

--- app/src/main/java/com/example/nastool/MainActivity.kt
+++ app/src/main/java/com/example/nastool/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.nastool
 import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.net.Uri
 import android.net.http.SslError
 import android.os.Bundle
@@ -10,19 +11,31 @@ import android.util.Log
 import android.view.KeyEvent
 import android.view.View
 import android.webkit.*
+import android.widget.Button
+import android.widget.EditText
 import android.widget.LinearLayout
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat.startActivity
 
 
 class MainActivity : AppCompatActivity() {
+    val TAG: String = "MainActivity"
     private lateinit var myWebView: WebView
-    val hostUrl: String = "http://192.168.137.1:3000"
+    private lateinit var myChangeUrl: Button
+    private lateinit var myTextUrl: TextView
+    private lateinit var myInputUrl: EditText
+    private lateinit var mySharedPreferences: SharedPreferences
+    private val buttonTextChange: String = "更改网址"
+    private val buttonTextSave: String = "保存更改"
+    var hostUrl: String = "" //"http://192.168.137.1:3000"
     val statusBarHeight: Int = 40
     var toCssTop: Int = 5
     var firstUpdated: Boolean = true
 
 
+    @SuppressLint("MissingInflatedId", "CommitPrefEdits")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -41,11 +54,47 @@ class MainActivity : AppCompatActivity() {
         val logo: LinearLayout = findViewById(R.id.image_logo)
         logo.visibility = View.GONE
     }
-    
+
+    private fun gotoUrl() {
+        if (hostUrl != "") {
+            myWebView.loadUrl(hostUrl)
+        } else {
+            myChangeUrl.visibility = View.VISIBLE
+        }
+    }
+
     @SuppressLint("SetJavaScriptEnabled")
     private fun initialize() {
-        myWebView = findViewById(R.id.webview_ding)
+        mySharedPreferences = getSharedPreferences("nasTool", MODE_PRIVATE)
+        hostUrl = mySharedPreferences.getString("hostUrl", "") ?: return
 
+        myChangeUrl = findViewById(R.id.button_change_url)
+        myTextUrl = findViewById(R.id.text_url)
+        myInputUrl = findViewById(R.id.input_url)
+        myChangeUrl.setOnClickListener {
+            Log.d(TAG, "initialize: " + myChangeUrl.text.toString())
+            if (myChangeUrl.text.toString() == buttonTextChange) {
+                // 更改网址时 显示输入框
+                myInputUrl.visibility = View.VISIBLE
+                myTextUrl.visibility = View.VISIBLE
+                myChangeUrl.text = buttonTextSave
+            } else if (myChangeUrl.text.toString() == buttonTextSave) {
+                // 保存
+                hostUrl = myInputUrl.text.toString()
+                if (hostUrl != "") {
+                    myInputUrl.visibility = View.GONE
+                    myTextUrl.visibility = View.GONE
+                    myChangeUrl.text = buttonTextChange
+                    mySharedPreferences.edit().putString("hostUrl", hostUrl).apply()
+                    gotoUrl()
+                    Toast.makeText(applicationContext,"正在连接..",Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(applicationContext,"请输入网址> http(s)://....",Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+
+        myWebView = findViewById(R.id.webview_ding)
         val settings: WebSettings = myWebView.settings // webView 配置项
         settings.useWideViewPort = true // 是否启用对视口元标记的支持
         settings.javaScriptEnabled = true // 是否启用 JavaScript
@@ -59,14 +108,12 @@ class MainActivity : AppCompatActivity() {
         settings.builtInZoomControls = true // 是否应使用其内置的缩放机制
         // Hide the zoom controls for HONEYCOMB+
         settings.displayZoomControls = false  // 是否应显示屏幕缩放控件
-//        settings.allowFileAccessFromFileURLs =
-//            true // 是否应允许在文件方案 URL 上下文中运行的 JavaScript 访问来自其他文件方案 URL 的内容
-//        settings.allowUniversalAccessFromFileURLs =
-//            true // 是否应允许在文件方案URL上下文中运行的 JavaScript 访问任何来源的内容
-        myWebView.loadUrl(hostUrl) // 设置访问地址
+        settings.allowFileAccessFromFileURLs = true // 是否应允许在文件方案 URL 上下文中运行的 JavaScript 访问来自其他文件方案 URL 的内容
+        settings.allowUniversalAccessFromFileURLs = true // 是否应允许在文件方案URL上下文中运行的 JavaScript 访问任何来源的内容
         //myWebView.setDrawingCacheEnabled(true) // 启用或禁用图形缓存
         myWebView.webViewClient = WVViewClient(this, this@MainActivity) // 帮助 WebView 处理各种通知、请求事件
         myWebView.webChromeClient = WVChromeClient(this, this@MainActivity) // 处理解析，渲染网页
+        gotoUrl()
     }
 }
 
