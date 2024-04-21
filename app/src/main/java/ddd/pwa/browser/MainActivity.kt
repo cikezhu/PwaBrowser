d5e2652cc210f2b5680afa8d769d48a26f00e1fc
叮叮当 <604054726@qq.com>
2024-04-22 01:37:39 +0800

    增加 可自定义快捷方式的名称与图标

--- app/src/main/java/ddd/pwa/browser/MainActivity.kt
+++ app/src/main/java/ddd/pwa/browser/MainActivity.kt
@@ -5,24 +5,35 @@ import android.content.Intent
 import android.content.pm.ShortcutInfo
 import android.content.pm.ShortcutManager
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.drawable.ColorDrawable
 import android.graphics.drawable.Icon
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
+import android.view.LayoutInflater
 import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.contract.ActivityResultContracts.StartActivityForResult
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.coroutines.*
+import java.io.ByteArrayOutputStream
+import java.security.MessageDigest
 
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var shortcutLogo: Bitmap
+    private lateinit var shortcutIcon: ImageView
     private val mTAG: String = "MainActivity"
     private val mScope = MainScope()
-    private val launcher = registerForActivityResult(
+    private val webViewLauncher = registerForActivityResult(
         StartActivityForResult()
     ) { result: ActivityResult ->
         if (result.resultCode == RESULT_OK) {
@@ -33,11 +44,27 @@ class MainActivity : AppCompatActivity() {
             val full = data.getBooleanExtra("full", true)
             @Suppress("DEPRECATION") val logo = data.getParcelableExtra<Bitmap>("logo")
             if (url !== null && name !== null && logo !== null) {
-                addShortcut(name, url, logo, full)
+                showShortcutCreationDialog(name, url, logo, full)
             }
         }
     }
-
+    private val iconLauncher = registerForActivityResult(
+        StartActivityForResult()
+    ) { result: ActivityResult ->
+        if (result.resultCode == RESULT_OK) {
+            val data = result.data
+            if (data != null) {
+                val selectedImageUri: Uri? = data.data
+                selectedImageUri?.let {
+                    val inputStream = this.contentResolver.openInputStream(it)
+                    shortcutLogo = BitmapFactory.decodeStream(inputStream)
+                    shortcutIcon.setImageBitmap(shortcutLogo)
+                    inputStream?.close()
+                }
+            }
+        }
+    }
+    
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -63,7 +90,7 @@ class MainActivity : AppCompatActivity() {
                 val intent = Intent(this@MainActivity, WebViewActivity::class.java)
                 intent.putExtra("mode", LAUNCH_MODE.GET_URL_DETAIL.intValue)
                 intent.putExtra("url", hostUrl)
-                launcher.launch(intent)
+                webViewLauncher.launch(intent)
             } else {
                 Toast.makeText(applicationContext,"请输入网址> http(s)://....",Toast.LENGTH_SHORT).show()
             }
@@ -71,23 +98,86 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun addShortcut(name: String, url: String, logo: Bitmap, full: Boolean) {
-        // 创建快捷方式
-        val shortcutInfo = ShortcutInfo.Builder(applicationContext, url)
-            .setShortLabel(name)
-            .setIcon(Icon.createWithBitmap(logo))
-            .setIntent(Intent(applicationContext, LauncherShortcutActivity::class.java).apply {
-                action = Intent.ACTION_MAIN
-                putExtra("mode", LAUNCH_MODE.SHOW_URL_PAGE.intValue)
-                putExtra("url", url)
-                putExtra("name", name)
-                putExtra("full", full)
-            })
-            .build()
         // 添加快捷方式到桌面
         mScope.launch(Dispatchers.Main) {
+            // 创建快捷方式
+            val shortcutInfo = ShortcutInfo.Builder(applicationContext, LAUNCH_MODE.SHOW_URL_PAGE.intValue.toString() + name + url + generateUniqueValueFromBitmap(logo))
+                .setShortLabel(name)
+                .setIcon(Icon.createWithBitmap(logo))
+                .setIntent(Intent(applicationContext, LauncherShortcutActivity::class.java).apply {
+                    action = Intent.ACTION_MAIN
+                    putExtra("mode", LAUNCH_MODE.SHOW_URL_PAGE.intValue)
+                    putExtra("url", url)
+                    putExtra("name", name)
+                    putExtra("full", full)
+                })
+                .build()
             val shortcutManager = getSystemService(ShortcutManager::class.java)
             shortcutManager.requestPinShortcut(shortcutInfo, null)
         }
     }
 
+    private fun generateUniqueValueFromBitmap(logo: Bitmap): String {
+        val byteArrayOutputStream = ByteArrayOutputStream()
+        logo.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream)
+        val byteArray = byteArrayOutputStream.toByteArray()
+
+        val messageDigest = MessageDigest.getInstance("MD5")
+        messageDigest.update(byteArray)
+        val digest = messageDigest.digest()
+
+        // 将字节数组转换为十六进制字符串
+        val hexString = StringBuilder()
+        for (byte in digest) {
+            val hex = Integer.toHexString(0xFF and byte.toInt())
+            if (hex.length == 1) {
+                hexString.append('0')
+            }
+            hexString.append(hex)
+        }
+
+        return hexString.toString()
+    }
+    private fun showShortcutCreationDialog(name: String, url: String, logo: Bitmap, full: Boolean) {
+        shortcutLogo = logo
+        val alertDialogBuilder = AlertDialog.Builder(this)
+        // 创建布局
+        val layoutInflater = LayoutInflater.from(this)
+        val dialogView = layoutInflater.inflate(R.layout.dialog_shortcut_creation, null)
+        val nameInput = dialogView.findViewById<EditText>(R.id.shortcut_name)
+        nameInput.setText(name)
+        // 选择图标的按钮
+        shortcutIcon = dialogView.findViewById(R.id.shortcut_icon)
+        shortcutIcon.setImageBitmap(logo)
+        shortcutIcon.setOnClickListener {
+            // 启动图片选择器
+            val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            iconLauncher.launch(intent)
+        }
+        // 设置弹窗内容
+        alertDialogBuilder.setView(dialogView)
+            .setPositiveButton("确定") { dialog, _ ->
+                val newName = nameInput.text.toString()
+                if (newName.isNotEmpty()) {
+                    addShortcut(newName, url, shortcutLogo, full)
+                }
+                dialog.dismiss()
+            }
+            .setNegativeButton("取消") { dialog, _ ->
+                dialog.dismiss()
+            }
+        // 显示弹窗
+        val alertDialog = alertDialogBuilder.create()
+        alertDialog.window?.setBackgroundDrawable(ColorDrawable(0))
+        alertDialog.show()
+        // 确定按钮
+        val shortcutConfirm = dialogView.findViewById<Button>(R.id.shortcut_confirm)
+        shortcutConfirm.setOnClickListener {
+            val newName = nameInput.text.toString()
+            if (newName.isNotEmpty()) {
+                addShortcut(newName, url, shortcutLogo, full)
+            }
+            alertDialog.dismiss()
+        }
+    }
 }
